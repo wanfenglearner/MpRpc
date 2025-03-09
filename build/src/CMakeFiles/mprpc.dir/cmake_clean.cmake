c14a55a0765588b6f9a1054f0d7812f8df95ae82
robin <2200726897@qq.com>
2025-03-09 21:34:06 +0800

    2025-3-9

---
+++ build/src/CMakeFiles/mprpc.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../lib/libmprpc.pdb"
+  "../../lib/libmprpc.so"
+  "CMakeFiles/mprpc.dir/src/mprpcapplication.cpp.o"
+  "CMakeFiles/mprpc.dir/src/mprpcapplication.cpp.o.d"
+  "CMakeFiles/mprpc.dir/src/mprpcconfig.cpp.o"
+  "CMakeFiles/mprpc.dir/src/mprpcconfig.cpp.o.d"
+  "CMakeFiles/mprpc.dir/src/rpcprovider.cpp.o"
+  "CMakeFiles/mprpc.dir/src/rpcprovider.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mprpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
